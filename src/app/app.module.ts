5fb12fd8cc44c6ab3082a8245f7b7b67b60a3a88
fabio damas <fabio.damas@gmail.com>
2020-06-17 22:00:43 -0300

    elaboracao do componente caixa de entrada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import	{HeaderComponent}	from	'./header/header.component';
 import { FormsModule } from '@angular/forms';
+import { CaixaDeEntradaComponent } from './modules/caixa-de-entrada/caixa-de-entrada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CaixaDeEntradaComponent
   ],
   imports: [
     BrowserModule,
