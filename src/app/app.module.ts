8873f84b85085a5ceed7e71b4583841523b56b40
fabio damas <fabio.damas@gmail.com>
2020-06-17 22:16:57 -0300

    Roteamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { CaixaDeEntradaComponent } from './modules/caixa-de-entrada/caixa-de-entrada.component';
 import { LoginComponent } from './modules/login/login.component';
 import { CadastroComponent } from './modules/cadastro/cadastro.component';
+import {ModuloRoteamento } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { CadastroComponent } from './modules/cadastro/cadastro.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule    
+    FormsModule,
+    ModuloRoteamento   
   ],
   providers: [],
   bootstrap: [AppComponent]
